74c3c3fff64546aa4721d8690fbb90101af63c56
Gustavo Tijerino <busg26@gmail.com>
2022-03-22 01:03:01 -0500

    Updated JS

---
+++ assets/js/result.js
@@ -0,0 +1,83 @@
+let sbmtBttn = document.getElementById('bookBttn')
+var bookBox= document.getElementById("book-title");
+var drinkTitle= document.getElementById("cocktail-name");
+
+
+let results = []
+
+
+
+
+
+
+function getApi() {
+
+  
+
+  var selectInput = document.getElementById("format-input").value;
+
+    // fetch request gets a list of all the repos for the node.js organization
+    var requestUrl = 'http://openlibrary.org/search.json?subject='+ selectInput;
+  console.log('requestUrl',requestUrl)
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+     
+      for(let i = 0; i <= 0; i++ ){
+        let allTheBooks = data.docs[i].title
+        
+
+        // console.log(allTheBooks);
+        
+         var listItem = document.createElement('li');
+         listItem.textContent = allTheBooks;
+         listItem.setAttribute('style', 'color:white');
+         console.log(listItem)
+         bookBox.appendChild(listItem)
+        
+      }
+        
+    });
+}
+
+function getApi2(){
+
+fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=scotch")
+  .then(function(response){
+    return response.json()
+  })
+  .then( function(data){
+    console.log(data)
+
+       
+    //no more than 15 drinks
+    for(var i = 0; i<= 0; i++){
+      let allDrinks = data.drinks[i].strDrink
+      // console.log('drink',data.drinks[i].strDrink)
+
+     //puts list into "your cocktail is"
+      let drinkList = document.createElement('li');
+      drinkList.textContent = allDrinks
+     drinkList.setAttribute('style', 'color:white');
+      drinkTitle.appendChild(drinkList)
+    }
+    
+  })
+}
+function reset(){
+    bookBox.innerText = ''
+    drinkTitle.innerText = ''
+  }
+  
+  sbmtBttn.addEventListener('click', function(event){
+      event.preventDefault()
+
+
+      getApi()
+      getApi2()
+      reset()
+      
+    })
+
