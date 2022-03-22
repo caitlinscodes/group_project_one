c01874780a876917262bac0a679e75df01ff8da4
Gustavo Tijerino <busg26@gmail.com>
2022-03-22 12:00:53 -0500

    I have modified js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,60 +1,17 @@
-let sbmtBttn = document.getElementById('bookBttn')
+
 const ageBttn = document.getElementById('ageBttn')
-const introCard = document.getElementById('introCard')
-const selectionBox = document.getElementById('selectionBox')
-const radioButtons = document.querySelectorAll('input[name="question"]');
 
 function askAge(radioInput){
-    if(radioInput.checked){
-     
-        ageBttn.disabled = false;
-    }
+  if(radioInput.checked){
+    
+    ageBttn.disabled = false;
+  }
 }
 
 
 ageBttn.addEventListener('click', () => {
-  introCard.setAttribute('style', 'display:none');
-  selectionBox.setAttribute('style', 'visibility:visible');
-  // document.getElementsByName('questoion')[0].focus;
+window.location.assign('index2.html');
+ 
   
 });
 
-function getApi(e) {
-
-  e.preventDefault()
-
-  var selectInput = document.getElementById("format-input").value;
-
-  localStorage.setItem('selectInput', selectInput);
-
-    // fetch request gets a list of all the repos for the node.js organization
-    var requestUrl = 'http://openlibrary.org/search.json?subject=' + selectInput;
-  console.log('requestUrl',requestUrl)
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        localStorage.setItem('genre',selectInput);
-       
-        if (!window.location.href.includes("/index2.html")) {
-          window.open("index2.html", "_parent")
-            return data
-          } 
-          
-          var bookTitle= document.getElementById("book-title");
-          
-          
-          for(var i = 0; i< 15; i++){
-            console.log('title',data.docs[i].title)
-            
-            var listItem = document.createElement('li');
-            listItem.textContent = data.docs[i].title;
-            console.log(listItem)
-            bookTitle.appendChild(listItem)
-          }
-          
-      });
-  }
-
-  bookBttn.addEventListener('click', getApi);
--- assets/javascript/script2.js
+++ assets/javascript/script2.js
@@ -1,57 +1,113 @@
+let sbmtBttn = document.getElementById('bookBttn')
+var bookBox= document.getElementById("book-title");
+var drinkTitle= document.getElementById("cocktail-name");
 
+function getApi() {
+  
+  const selectInput = document.getElementById("format-input").value;
+  
+  if (!selectInput) {
+        
+    alert('You need to pick Genre!')
+    
+  }   else {
+    
+  }
 
-//pull genre from local storage
-var bookGenre = localStorage.getItem('genre');
-var selectInput = localStorage.getItem('selectInput')
-console.log(bookGenre);
-
-//pull book API
-var requestUrl = 'http://openlibrary.org/search.json?subject=' + bookGenre;
-console.log('requestUrl',requestUrl)
+  
+  // fetch request gets a list of all the repos for the node.js organization
+  var requestUrl = 'http://openlibrary.org/search.json?subject='+ selectInput;
+  console.log('requestUrl',requestUrl)
   fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      localStorage.setItem('genre',selectInput);
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
      
-        var bookTitle= document.getElementById("book-title");
+    for(let i = 0; i <= 0; i++ ){
+        let allTheBooks = data.docs[i].title
         
         
-        for(var i = 0; i< 15; i++){
-          console.log('title',data.docs[i].title)
-          
-          var listItem = document.createElement('li');
-          listItem.textContent = data.docs[i].title;
-          console.log(listItem)
-          bookTitle.appendChild(listItem)
+        //  Math.floor(Math.random() * (all + 1) + min);
+        
+        
+         var listItem = document.createElement('li');
+         listItem.textContent = allTheBooks;
+         listItem.setAttribute('style', 'color:white');
+         console.log(listItem)
+         bookBox.appendChild(listItem)
+         
+         localStorage.setItem("notPotato", JSON.stringify(allTheBooks))
         }
+      });
+    }
+  
+    
+function getApi2(){
+  const cocktailInput = document.getElementById("cocktail-input").value
+
+  
+  if (!cocktailInput) {
         
-    });
+    alert('You need to pick Cocktail Base!')
+    
+  }   
 
 
-
-
-
-//gets drinks from API
-fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=scotch")
-  .then(function(response){
-    return response.json()
+  
+fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + cocktailInput)
+  .then(function(choice){
+    return choice.json();
   })
   .then( function(data){
     console.log(data)
 
-    var drinkTitle= document.getElementById("cocktail-name");
        
     //no more than 15 drinks
-    for(var i = 0; i< 15; i++){
-      console.log('drink',data.drinks[i].strDrink)
+    for(var i = 0; i<= 0; i++){
+      let allDrinks = data.drinks[i].strDrink
+      // console.log('drink',data.drinks[i].strDrink)
 
      //puts list into "your cocktail is"
-      var listItem = document.createElement('li');
-      listItem.textContent = data.drinks[i].strDrink;
-      console.log(listItem)
-      drinkTitle.appendChild(listItem)
+      let drinkList = document.createElement('li');
+      drinkList.textContent = allDrinks
+     drinkList.setAttribute('style', 'color:white');
+     drinkTitle.appendChild(drinkList)
+
+      localStorage.setItem("notPotato2", JSON.stringify(allDrinks))
     }
     
-})
+  })
+}
+
+function reset(){
+    bookBox.innerText = ''
+    drinkTitle.innerText = ''
+  }
+  
+  sbmtBttn.addEventListener('click', function(event){
+      event.preventDefault()
+      
+      
+      getApi()
+      getApi2()
+      reset()
+      
+      // if (!selectInput || !cocktailInput) {
+        
+      //   alert('You need to pick Genre!');
+      
+      // }  
+      
+    })
+
+    function storage(){
+
+      document.getElementById('result-history')
+
+
+
+
+
+
+    }
